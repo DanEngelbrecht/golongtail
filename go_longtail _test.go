234ae06072a2a7f825d93c05ac7ff4299c2d1329
Dan Engelbrecht <dan.engelbrecht@embark-studios.com>
2019-12-10 14:21:16 +0100

    CreateVersionIndexFromFolder with dummy test

--- go_longtail _test.go
+++ go_longtail _test.go
@@ -1,10 +1,52 @@
 package golongtail
 
-import "testing"
+import (
+	"testing"
+	"unsafe"
+)
 
+type progressData struct {
+	inited     bool
+	oldPercent int
+	task       string
+	t          *testing.T
+}
+
+func progress(context interface{}, total int, current int) {
+	p := context.(*progressData)
+	if current < total {
+		if !p.inited {
+			p.t.Logf("%s: ", p.task)
+			p.inited = true
+		}
+		percentDone := (100 * current) / total
+		if (percentDone - p.oldPercent) >= 5 {
+			p.t.Logf("%d%% ", percentDone)
+			p.oldPercent = percentDone
+		}
+		return
+	}
+	if p.inited {
+		if p.oldPercent != 100 {
+			p.t.Logf("100%%")
+		}
+		p.t.Logf(" Done\n")
+	}
+}
+
+func TestCreateVersionIndexFromFolder(t *testing.T) {
+	vi := CreateVersionIndexFromFolder("C:\\Temp\\longtail\\local\\WinEditor\\git2f7f84a05fc290c717c8b5c0e59f8121481151e6_Win64_Editor", MakeProgressProxy(progress, &progressData{task: "Indexing", t: t}))
+	if vi == nil {
+		t.Errorf("CreateVersionIndexFromFolder() = nil, want !nil")
+	}
+	LongtailFree(unsafe.Pointer(vi))
+}
+
+/*
 func TestChunkFolder(t *testing.T) {
 	expected := int32(194061)
 	if ret := ChunkFolder("C:\\Temp\\longtail\\local\\WinEditor\\git2f7f84a05fc290c717c8b5c0e59f8121481151e6_Win64_Editor"); ret != expected {
 		t.Errorf("ChunkFolder() = %q, want %q", ret, expected)
 	}
 }
+*/
--- go_longtail.go
+++ go_longtail.go
@@ -9,56 +9,78 @@ package golongtail
 // void progressProxy(void* context, uint32_t total_count, uint32_t done_count);
 import "C"
 import (
-	"fmt"
 	"unsafe"
 	"runtime"
 
 	"github.com/mattn/go-pointer"
 )
 
-type progressFunc func(context interface{}, total int, current int)
+//ProgressFunc ...
+type ProgressFunc func(context interface{}, total int, current int)
 
-type progressProxyData struct {
-	progressFunc progressFunc
-	context      interface{}
+//ProgressProxyData ...
+type ProgressProxyData struct {
+	ProgressFunc ProgressFunc
+	Context      interface{}
 }
 
-func makeProgressProxy(progressFunc progressFunc, context interface{}) progressProxyData {
-	return progressProxyData{progressFunc, context}
+//MakeProgressProxy create data for progress function
+func MakeProgressProxy(progressFunc ProgressFunc, context interface{}) ProgressProxyData {
+	return ProgressProxyData{progressFunc, context}
 }
 
-type progressData struct {
-	oldPercent int
-	task       string
+//LongtailFree ...
+func LongtailFree(data unsafe.Pointer) {
+	C.Longtail_Free(data)
 }
 
-func progress(context interface{}, total int, current int) {
-	p := context.(*progressData)
-	if current < total {
-		if p.oldPercent == 0 {
-			fmt.Printf("%s: ", p.task)
-		}
-		percentDone := (100 * current) / total
-		if (percentDone - p.oldPercent) >= 5 {
-			fmt.Printf("%d%% ", percentDone)
-			p.oldPercent = percentDone
-		}
-		return
+//GetVersionIndex ...
+func CreateVersionIndexFromFolder(folderPath string, progressProxyData ProgressProxyData) *C.struct_VersionIndex {
+	progressContext := pointer.Save(&progressProxyData)
+	defer pointer.Unref(progressContext)
+
+	cFolderPath := C.CString(folderPath)
+	defer C.free(unsafe.Pointer(cFolderPath))
+
+	fs := C.CreateFSStorageAPI()
+	defer C.DestroyStorageAPI(fs)
+
+	hs := C.CreateMeowHashAPI()
+	defer C.DestroyHashAPI(hs)
+
+	jb := C.CreateBikeshedJobAPI(C.uint32_t(runtime.NumCPU()))
+	defer C.DestroyJobAPI(jb)
+
+	fileInfos := C.GetFilesRecursively(fs, cFolderPath)
+	defer C.Longtail_Free(unsafe.Pointer(fileInfos))
+
+	compressionTypes := make([]C.uint32_t, int(*fileInfos.m_Paths.m_PathCount))
+	for i := 1; i < int(*fileInfos.m_Paths.m_PathCount); i++ {
+		compressionTypes[i] = 0	// Need to change LIZARD_DEFAULT_COMPRESSION_TYPE form macro!
 	}
-	if p.oldPercent != 0 {
-		if p.oldPercent != 100 {
-			fmt.Printf("100%%")
-		}
-		fmt.Print(" Done\n")
-	}
+
+	vindex := C.CreateVersionIndex(
+		fs,
+		hs,
+		jb,
+		(C.JobAPI_ProgressFunc)(C.progressProxy),
+		progressContext,
+		cFolderPath,
+		(*C.struct_Paths)(&fileInfos.m_Paths),
+		fileInfos.m_FileSizes,
+		(*C.uint32_t)(unsafe.Pointer(&compressionTypes[0])),
+		C.uint32_t(32768))
+
+	return vindex
 }
 
+/*
 //ChunkFolder hello
-func ChunkFolder(folder_path string) int32 {
+func ChunkFolder(folderPath string) int32 {
 	progressProxy := makeProgressProxy(progress, &progressData{task: "Indexing"})
 	c := pointer.Save(&progressProxy)
 
-	path := C.CString(folder_path)
+	path := C.CString(folderPath)
 	defer C.free(unsafe.Pointer(path))
 
 	fs := C.CreateFSStorageAPI()
@@ -97,23 +119,9 @@ func ChunkFolder(folder_path string) int32 {
 
 	return chunkCount
 }
-
-/*
-func main() {
-
-	size := C.GetVersionIndexSize(
-		10,
-		20,
-		30,
-		40)
-	println(size)
-
-
-	return
-}
 */
 //export progressProxy
 func progressProxy(progress unsafe.Pointer, total C.uint32_t, done C.uint32_t) {
-	progressProxy := pointer.Restore(progress).(*progressProxyData)
-	progressProxy.progressFunc(progressProxy.context, int(total), int(done))
+	progressProxy := pointer.Restore(progress).(*ProgressProxyData)
+	progressProxy.ProgressFunc(progressProxy.Context, int(total), int(done))
 }

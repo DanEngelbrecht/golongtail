51c7c66ffff683536fe995bf773e52ec4f57d968
Dan Engelbrecht <dan@engelbrecht.com>
2020-01-16 00:44:56 +0100

    cmd.go -> longtail.go Only clear obj files on linux when building c library

--- longtail/import/build_lib.sh
+++ longtail/import/build_lib.sh
@@ -9,7 +9,7 @@ MEMSTORAGE_SRC="../lib/memstorage/*.c"
 MEOWHASH_SRC="../lib/meowhash/*.c"
 LIZARD_SRC="../lib/lizard/*.c ../lib/lizard/ext/*.c ../lib/lizard/ext/entropy/*.c ../lib/lizard/ext/xxhash/*.c"
 BROTLI_SRC="../lib/brotli/*.c ../lib/brotli/ext/common/*.c ../lib/brotli/ext/dec/*.c ../lib/brotli/ext/enc/*.c ../lib/brotli/ext/fuzz/*.c"
-rm *
+rm *.o
 gcc -c -std=gnu99 -m64 -O3 -pthread -msse4.1 -maes -Isrc ../src/*.c ../src/ext/*.c ../lib/*.c $BIKESHED_SRC $BLAKE2_SRC $FILESTORAGE_SRC $MEMSTORAGE_SRC $MEOWHASH_SRC $LIZARD_SRC $BROTLI_SRC
 ar rc ../longtail_lib.a *.o
 popd

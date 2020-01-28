9e284e2595e33eceea6e61f9b8102664956029f1
Dan Engelbrecht <dan@engelbrecht.com>
2020-01-28 12:56:26 +0100

    Removed bad header include path. ar verbose

--- lib/import/build_lib.sh
+++ lib/import/build_lib.sh
@@ -36,6 +36,6 @@ LIZARD_SRC="../lib/lizard/*.c ../lib/lizard/ext/*.c ../lib/lizard/ext/entropy/*.
 BROTLI_SRC="../lib/brotli/*.c ../lib/brotli/ext/common/*.c ../lib/brotli/ext/dec/*.c ../lib/brotli/ext/enc/*.c ../lib/brotli/ext/fuzz/*.c"
 ZLIB_SRC="../lib/zstd/*.c ../lib/zstd/ext/common/*.c ../lib/zstd/ext/compress/*.c ../lib/zstd/ext/decompress/*.c"
 rm *.o
-gcc -c -v -std=gnu99 -m64 -O3 -pthread -msse4.1 -maes -Isrc ../src/*.c ../src/ext/*.c ../lib/*.c $BIKESHED_SRC $BLAKE2_SRC $BLAKE3_SRC $FILESTORAGE_SRC $MEMSTORAGE_SRC $MEOWHASH_SRC $LIZARD_SRC $BROTLI_SRC $ZLIB_SRC
+gcc -c -std=gnu99 -m64 -O3 -pthread -msse4.1 -maes ../src/*.c ../src/ext/*.c ../lib/*.c $BIKESHED_SRC $BLAKE2_SRC $BLAKE3_SRC $FILESTORAGE_SRC $MEMSTORAGE_SRC $MEOWHASH_SRC $LIZARD_SRC $BROTLI_SRC $ZLIB_SRC
 popd
-ar -v rc $LIB_TARGET obj/*.o
+ar cr -v $LIB_TARGET obj/*.o

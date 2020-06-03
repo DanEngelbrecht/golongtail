1b29e98861cd920961bb3412dbd87e1cd72c6f95
Dan Engelbrecht <dan@engelbrecht.com>
2020-06-03 12:53:05 +0200

    compiler flags

--- longtaillib/longtail_blake3_avx512.go
+++ longtaillib/longtail_blake3_avx512.go
@@ -1,5 +1,5 @@
 package longtaillib
 
-// #cgo CFLAGS: -g -std=gnu99 -m64 -pthread -mavx512vl -mavx512f -O3 -fno-asynchronous-unwind-tables
+// #cgo CFLAGS: -g -std=gnu99 -m64 -pthread -msse4.1 -mavx2 -mavx512vl -mavx512f -fno-asynchronous-unwind-tables -O3
 // #include "longtail/lib/blake3/ext/blake3_avx512.c"
 import "C"

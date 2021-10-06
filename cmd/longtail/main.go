c92530c38ec8d9149652862d5b732a0bd20c2658
Dan Engelbrecht <dan@engelbrecht.com>
2021-10-06 12:48:48 +0200

    Nicer progress bar

--- cmd/longtail/main.go
+++ cmd/longtail/main.go
@@ -79,29 +79,63 @@ func (a *assertData) OnAssert(expression string, file string, line int) {
 }
 
 type progressData struct {
-	inited bool
-	task   string
+	inited    bool
+	startTime time.Time
+	lastTime  time.Time
+	last      uint32
+	task      string
 }
 
 func (p *progressData) OnProgress(totalCount uint32, doneCount uint32) {
+	endChar := ""
 	if doneCount == totalCount {
-		if p.inited {
-			fmt.Fprintf(os.Stderr, "100%%")
-			fmt.Fprintf(os.Stderr, " Done\n")
+		if !p.inited {
+			return
+			//			fmt.Fprintf(os.Stderr, "\r%s: %d%%\n", p.task, 100)
 		}
-		return
+		endChar = "\n"
+		//		return
 	}
-	if !p.inited {
-		fmt.Fprintf(os.Stderr, "%s: ", p.task)
-		p.inited = true
+
+	etaString := ""
+	if p.inited {
+		if doneCount != totalCount {
+			fractionDoneThisRound := float64(doneCount-p.last) / float64(totalCount)
+			timeThisRound := time.Since(p.lastTime)
+			fractionLeft := float64(totalCount-doneCount) / float64(totalCount)
+			fractionEta := float64(timeThisRound.Seconds()) / fractionDoneThisRound * fractionLeft
+			averageRate := float64(doneCount) / time.Since(p.startTime).Seconds()
+			averageEta := (float64(totalCount) - float64(doneCount)) / averageRate
+			weightedEta := ((fractionEta + (averageEta * 2)) / 3)
+			eta := (time.Duration(weightedEta) * time.Second).String()
+			etaString = fmt.Sprintf(":%s", eta)
+		}
 	}
-	percentDone := (100 * doneCount) / totalCount
-	fmt.Fprintf(os.Stderr, "%d%% ", percentDone)
+	p.lastTime = time.Now()
+
+	p.inited = true
+	percentDone := int((100 * doneCount) / totalCount)
+
+	progressBarFullLength := 50
+	progressBarCount := int(progressBarFullLength * percentDone / 100)
+	elapsed := (time.Duration(time.Since(p.startTime).Seconds()) * time.Second).String()
+
+	timeString := fmt.Sprintf("%s%s", elapsed, etaString)
+
+	fmt.Fprintf(os.Stderr,
+		"\r%s %3d%%: |%s%s|: [%s]        %s\r",
+		p.task,
+		percentDone,
+		strings.Repeat("█", progressBarCount), strings.Repeat(" ", progressBarFullLength-progressBarCount),
+		timeString,
+		endChar)
+
+	p.last = doneCount
 }
 
 func CreateProgress(task string) longtaillib.Longtail_ProgressAPI {
-	baseProgress := longtaillib.CreateProgressAPI(&progressData{task: task})
-	return longtaillib.CreateRateLimitedProgressAPI(baseProgress, 5)
+	baseProgress := longtaillib.CreateProgressAPI(&progressData{task: task, startTime: time.Now()})
+	return longtaillib.CreateRateLimitedProgressAPI(baseProgress, 2)
 }
 
 type timeStat struct {

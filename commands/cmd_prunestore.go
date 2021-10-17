ff372eb606bc708dc713c88ad8f3d6f7a1c0c7b1
Dan Engelbrecht <dan@engelbrecht.com>
2021-10-17 21:58:46 +0200

    Make prune store block usage anailsys more concurrent

--- commands/cmd_prunestore.go
+++ commands/cmd_prunestore.go
@@ -14,6 +14,245 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type pruneOneResult struct {
+	err    error
+	blocks []uint64
+}
+
+func readPruneVersion(
+	sourceFilePath string,
+	versionLocalStoreIndexFilePath string,
+	writeVersionLocalStoreIndex bool) (longtaillib.Longtail_VersionIndex, longtaillib.Longtail_StoreIndex, error) {
+	const fname = "readPruneVersion"
+	vbuffer, err := longtailutils.ReadFromURI(sourceFilePath)
+	if err != nil {
+		return longtaillib.Longtail_VersionIndex{}, longtaillib.Longtail_StoreIndex{}, errors.Wrap(err, fname)
+	}
+	versionIndex, err := longtaillib.ReadVersionIndexFromBuffer(vbuffer)
+	if err != nil {
+		err = errors.Wrapf(err, "Cant parse version index from `%s`", sourceFilePath)
+		return longtaillib.Longtail_VersionIndex{}, longtaillib.Longtail_StoreIndex{}, errors.Wrap(err, fname)
+	}
+
+	var storeIndex longtaillib.Longtail_StoreIndex
+	if versionLocalStoreIndexFilePath != "" && !writeVersionLocalStoreIndex {
+		sbuffer, err := longtailutils.ReadFromURI(versionLocalStoreIndexFilePath)
+		if err == nil {
+			storeIndex, err = longtaillib.ReadStoreIndexFromBuffer(sbuffer)
+			if err != nil {
+				err = errors.Wrapf(err, "Cant parse store index from `%s`", versionLocalStoreIndexFilePath)
+				versionIndex.Dispose()
+				return longtaillib.Longtail_VersionIndex{}, longtaillib.Longtail_StoreIndex{}, errors.Wrap(err, fname)
+			}
+			err = longtaillib.ValidateStore(storeIndex, versionIndex)
+			if err != nil {
+				storeIndex.Dispose()
+				versionIndex.Dispose()
+				err = errors.Wrapf(err, "Cant validate store index for `%s`", versionLocalStoreIndexFilePath)
+				return longtaillib.Longtail_VersionIndex{}, longtaillib.Longtail_StoreIndex{}, errors.Wrap(err, fname)
+			}
+		}
+	}
+	return versionIndex, storeIndex, nil
+}
+
+func pruneOne(
+	remoteStore longtaillib.Longtail_BlockStoreAPI,
+	sourceFilePath string,
+	versionLocalStoreIndexFilePath string,
+	validateVersions bool,
+	skipInvalidVersions bool,
+	writeVersionLocalStoreIndex bool,
+	dryRun bool,
+	outResults chan<- pruneOneResult) {
+	const fname = "pruneOne"
+	log := logrus.WithFields(logrus.Fields{
+		"fname": fname,
+	})
+	log.Debug(fname)
+
+	result := pruneOneResult{}
+	defer func() {
+		outResults <- result
+	}()
+
+	versionIndex, existingStoreIndex, err := readPruneVersion(sourceFilePath, versionLocalStoreIndexFilePath, writeVersionLocalStoreIndex)
+	if err != nil {
+		result.err = errors.Wrap(err, fname)
+		return
+	}
+	defer versionIndex.Dispose()
+	defer existingStoreIndex.Dispose()
+
+	if !existingStoreIndex.IsValid() {
+		neededChunks := versionIndex.GetChunkHashes()
+		existingStoreIndex, err = longtailutils.GetExistingStoreIndexSync(remoteStore, neededChunks, 0)
+		if err != nil {
+			result.err = errors.Wrap(err, fname)
+			return
+		}
+		defer existingStoreIndex.Dispose()
+
+		if validateVersions {
+			err = longtaillib.ValidateStore(existingStoreIndex, versionIndex)
+			if err != nil {
+				existingStoreIndex.Dispose()
+				if skipInvalidVersions {
+					log.Warnf("Data is missing for version `%s`", sourceFilePath)
+					result.err = nil
+					return
+				} else {
+					err = errors.Wrapf(err, "pruneStore: Data is missing for version `%s`", sourceFilePath)
+					result.err = errors.Wrap(err, fname)
+					return
+				}
+			}
+		}
+	}
+
+	if versionLocalStoreIndexFilePath != "" && writeVersionLocalStoreIndex && !dryRun {
+		sbuffer, err := longtaillib.WriteStoreIndexToBuffer(existingStoreIndex)
+		if err != nil {
+			existingStoreIndex.Dispose()
+			result.err = errors.Wrap(err, fname)
+			return
+		}
+		err = longtailutils.WriteToURI(versionLocalStoreIndexFilePath, sbuffer)
+		if err != nil {
+			existingStoreIndex.Dispose()
+			result.err = errors.Wrap(err, fname)
+			return
+		}
+	}
+
+	result = pruneOneResult{err: nil}
+	result.blocks = append(result.blocks, existingStoreIndex.GetBlockHashes()...)
+}
+
+func gatherBlocksToKeep(
+	numWorkerCount int,
+	storageURI string,
+	sourceFilePaths []string,
+	versionLocalStoreIndexFilePaths []string,
+	writeVersionLocalStoreIndex bool,
+	validateVersions bool,
+	skipInvalidVersions bool,
+	dryRun bool,
+	jobs longtaillib.Longtail_JobAPI) ([]uint64, error) {
+	const fname = "gatherBlocksToKeep"
+	log := logrus.WithFields(logrus.Fields{
+		"fname":                       fname,
+		"numWorkerCount":              numWorkerCount,
+		"storageURI":                  storageURI,
+		"writeVersionLocalStoreIndex": writeVersionLocalStoreIndex,
+		"validateVersions":            validateVersions,
+		"skipInvalidVersions":         skipInvalidVersions,
+		"dryRun":                      dryRun,
+	})
+	log.Debug(fname)
+	remoteStore, err := remotestore.CreateBlockStoreForURI(storageURI, "", jobs, numWorkerCount, 8388608, 1024, remotestore.ReadOnly)
+	if err != nil {
+		return nil, errors.Wrap(err, fname)
+	}
+	defer remoteStore.Dispose()
+
+	usedBlocks := make(map[uint64]uint32)
+
+	resultChannel := make(chan pruneOneResult, numWorkerCount)
+	activeWorkerCount := 0
+
+	progress := longtailutils.CreateProgress("Processing versions", 0)
+	defer progress.Dispose()
+
+	totalCount := uint32(len(sourceFilePaths))
+	completed := 0
+
+	for i, sourceFilePath := range sourceFilePaths {
+		versionLocalStoreIndexFilePath := ""
+		if len(versionLocalStoreIndexFilePaths) > 0 {
+			versionLocalStoreIndexFilePath = versionLocalStoreIndexFilePaths[i]
+		}
+
+		if activeWorkerCount == numWorkerCount {
+			result := <-resultChannel
+			completed++
+			if result.err == nil {
+				for _, h := range result.blocks {
+					usedBlocks[h] += 1
+				}
+			}
+			activeWorkerCount--
+		}
+
+		if sourceFilePath != "" {
+			go pruneOne(
+				remoteStore,
+				sourceFilePath,
+				versionLocalStoreIndexFilePath,
+				validateVersions,
+				skipInvalidVersions,
+				writeVersionLocalStoreIndex,
+				dryRun,
+				resultChannel)
+			activeWorkerCount++
+		}
+
+		for activeWorkerCount > 0 {
+			received := false
+			select {
+			case result := <-resultChannel:
+				completed++
+				if result.err == nil {
+					for _, h := range result.blocks {
+						usedBlocks[h] += 1
+					}
+				} else {
+					err = result.err
+				}
+				activeWorkerCount--
+				received = true
+			default:
+			}
+			if !received {
+				break
+			}
+		}
+
+		if err != nil {
+			break
+		}
+
+		progress.OnProgress(totalCount, uint32(completed))
+	}
+
+	for activeWorkerCount > 0 {
+		result := <-resultChannel
+		completed++
+		activeWorkerCount--
+		if result.err == nil {
+			for _, h := range result.blocks {
+				usedBlocks[h] += 1
+			}
+		} else {
+			err = result.err
+		}
+		progress.OnProgress(totalCount, uint32(completed))
+	}
+
+	if err != nil {
+		return nil, errors.Wrap(err, fname)
+	}
+
+	blockHashes := make([]uint64, len(usedBlocks))
+	i := 0
+	for k := range usedBlocks {
+		blockHashes[i] = k
+		i++
+	}
+
+	return blockHashes, nil
+}
+
 func pruneStore(
 	numWorkerCount int,
 	storageURI string,
@@ -43,17 +282,6 @@ func pruneStore(
 
 	jobs := longtaillib.CreateBikeshedJobAPI(uint32(numWorkerCount), 0)
 	defer jobs.Dispose()
-
-	storeMode := remotestore.ReadOnly
-	if !dryRun {
-		storeMode = remotestore.ReadWrite
-	}
-
-	remoteStore, err := remotestore.CreateBlockStoreForURI(storageURI, "", jobs, numWorkerCount, 8388608, 1024, storeMode)
-	if err != nil {
-		return storeStats, timeStats, errors.Wrap(err, fname)
-	}
-	defer remoteStore.Dispose()
 
 	setupTime := time.Since(setupStartTime)
 	timeStats = append(timeStats, longtailutils.TimeStat{"Setup", setupTime})
@@ -99,148 +327,37 @@ func pruneStore(
 		}
 	}
 
-	usedBlocks := make(map[uint64]uint32)
+	gatherBlocksToKeepStartTime := time.Now()
+	blocksToKeep, err := gatherBlocksToKeep(
+		numWorkerCount,
+		storageURI,
+		sourceFilePaths,
+		versionLocalStoreIndexFilePaths,
+		writeVersionLocalStoreIndex,
+		validateVersions,
+		skipInvalidVersions,
+		dryRun,
+		jobs)
 
-	batchCount := numWorkerCount
-	if batchCount > len(sourceFilePaths) {
-		batchCount = len(sourceFilePaths)
+	if err != nil {
+		return storeStats, timeStats, errors.Wrap(err, fname)
 	}
-	batchStart := 0
 
-	scanningForBlocksStartTime := time.Now()
-
-	batchErrors := make(chan error, batchCount)
-	progress := longtailutils.CreateProgress("Processing versions")
-	defer progress.Dispose()
-	for batchStart < len(sourceFilePaths) {
-		batchLength := batchCount
-		if batchStart+batchLength > len(sourceFilePaths) {
-			batchLength = len(sourceFilePaths) - batchStart
-		}
-		blockHashesPerBatch := make([][]uint64, batchLength)
-		for batchPos := 0; batchPos < batchLength; batchPos++ {
-			i := batchStart + batchPos
-			sourceFilePath := sourceFilePaths[i]
-			versionLocalStoreIndexFilePath := ""
-			if len(versionLocalStoreIndexFilePaths) > 0 {
-				versionLocalStoreIndexFilePath = versionLocalStoreIndexFilePaths[i]
-			}
-			go func(batchPos int, sourceFilePath string, versionLocalStoreIndexFilePath string) {
-				const fname = "batch"
-				log := logrus.WithFields(logrus.Fields{
-					"fname": fname,
-				})
-				log.Debug(fname)
-				vbuffer, err := longtailutils.ReadFromURI(sourceFilePath)
-				if err != nil {
-					batchErrors <- errors.Wrap(err, fname)
-					return
-				}
-				sourceVersionIndex, err := longtaillib.ReadVersionIndexFromBuffer(vbuffer)
-				if err != nil {
-					err = errors.Wrapf(err, "Cant parse version index from `%s`", sourceFilePath)
-					batchErrors <- errors.Wrap(err, fname)
-					return
-				}
-
-				var existingStoreIndex longtaillib.Longtail_StoreIndex
-				if versionLocalStoreIndexFilePath != "" && !writeVersionLocalStoreIndex {
-					sbuffer, err := longtailutils.ReadFromURI(versionLocalStoreIndexFilePath)
-					if err == nil {
-						existingStoreIndex, err = longtaillib.ReadStoreIndexFromBuffer(sbuffer)
-						if err != nil {
-							err = errors.Wrapf(err, "Cant parse store index from `%s`", versionLocalStoreIndexFilePath)
-							batchErrors <- errors.Wrap(err, fname)
-							return
-						}
-						err = longtaillib.ValidateStore(existingStoreIndex, sourceVersionIndex)
-						if err != nil {
-							existingStoreIndex.Dispose()
-						}
-					}
-				}
-				if !existingStoreIndex.IsValid() {
-					existingStoreIndex, err = longtailutils.GetExistingStoreIndexSync(remoteStore, sourceVersionIndex.GetChunkHashes(), 0)
-					if err != nil {
-						sourceVersionIndex.Dispose()
-						batchErrors <- errors.Wrap(err, fname)
-						return
-					}
-					err = longtaillib.ValidateStore(existingStoreIndex, sourceVersionIndex)
-					if err != nil {
-						existingStoreIndex.Dispose()
-						sourceVersionIndex.Dispose()
-						if dryRun {
-							log.Warnf("Data is missing in store `%s` for version `%s`", storageURI, sourceFilePath)
-							batchErrors <- nil
-						} else {
-							err = errors.Wrapf(err, "pruneStore: Data is missing in store `%s` for version `%s`", storageURI, sourceFilePath)
-							batchErrors <- errors.Wrap(err, fname)
-						}
-						return
-					}
-				}
-
-				blockHashesPerBatch[batchPos] = append(blockHashesPerBatch[batchPos], existingStoreIndex.GetBlockHashes()...)
-
-				if versionLocalStoreIndexFilePath != "" && writeVersionLocalStoreIndex && !dryRun {
-					sbuffer, err := longtaillib.WriteStoreIndexToBuffer(existingStoreIndex)
-					if err != nil {
-						existingStoreIndex.Dispose()
-						sourceVersionIndex.Dispose()
-						batchErrors <- errors.Wrap(err, fname)
-						return
-					}
-					err = longtailutils.WriteToURI(versionLocalStoreIndexFilePath, sbuffer)
-					if err != nil {
-						existingStoreIndex.Dispose()
-						sourceVersionIndex.Dispose()
-						batchErrors <- errors.Wrap(err, fname)
-						return
-					}
-				}
-				existingStoreIndex.Dispose()
-				sourceVersionIndex.Dispose()
-
-				batchErrors <- nil
-			}(batchPos, sourceFilePath, versionLocalStoreIndexFilePath)
-		}
-
-		for batchPos := 0; batchPos < batchLength; batchPos++ {
-			batchError := <-batchErrors
-			if batchError != nil {
-				return storeStats, timeStats, errors.Wrap(batchError, fname)
-			}
-			progress.OnProgress(uint32(len(sourceFilePaths)), uint32(batchStart+batchPos))
-		}
-		for batchPos := 0; batchPos < batchLength; batchPos++ {
-			for _, h := range blockHashesPerBatch[batchPos] {
-				usedBlocks[h] += 1
-			}
-		}
-
-		batchStart += batchLength
-	}
-	progress.OnProgress(uint32(len(sourceFilePaths)), uint32(len(sourceFilePaths)))
-
-	scanningForBlocksTime := time.Since(scanningForBlocksStartTime)
-	timeStats = append(timeStats, longtailutils.TimeStat{"Scanning", scanningForBlocksTime})
+	gatherBlocksToKeepTime := time.Since(gatherBlocksToKeepStartTime)
+	timeStats = append(timeStats, longtailutils.TimeStat{"Gather used blocks", gatherBlocksToKeepTime})
 
 	if dryRun {
-		log.Infof("Prune would keep %d blocks", len(usedBlocks))
+		fmt.Printf("Prune would keep %d blocks", len(blocksToKeep))
 		return storeStats, timeStats, nil
 	}
+	remoteStore, err := remotestore.CreateBlockStoreForURI(storageURI, "", jobs, numWorkerCount, 8388608, 1024, remotestore.ReadWrite)
+	if err != nil {
+		return storeStats, timeStats, errors.Wrap(err, fname)
+	}
+	defer remoteStore.Dispose()
 
 	pruneStartTime := time.Now()
-
-	blockHashes := make([]uint64, len(usedBlocks))
-	i := 0
-	for k := range usedBlocks {
-		blockHashes[i] = k
-		i++
-	}
-
-	prunedBlockCount, err := longtailutils.PruneBlocksSync(remoteStore, blockHashes)
+	prunedBlockCount, err := longtailutils.PruneBlocksSync(remoteStore, blocksToKeep)
 	if err != nil {
 		return storeStats, timeStats, errors.Wrap(err, fname)
 	}
@@ -250,12 +367,10 @@ func pruneStore(
 	log.Infof("Pruned %d blocks", prunedBlockCount)
 
 	flushStartTime := time.Now()
-
 	err = longtailutils.FlushStoreSync(&remoteStore)
 	if err != nil {
 		return storeStats, timeStats, errors.Wrap(err, fname)
 	}
-
 	flushTime := time.Since(flushStartTime)
 	timeStats = append(timeStats, longtailutils.TimeStat{"Flush", flushTime})
 
